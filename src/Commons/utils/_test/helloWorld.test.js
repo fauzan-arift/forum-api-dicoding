a249e729576456f1344a10122f4e410b16089da6
fauzan-arift <fauzan.ariftricahya@gmail.com>
2026-04-24 08:35:45 +0700

    feat: add hello world fail test feature

---
+++ src/Commons/utils/_test/helloWorld.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import helloWorld from '../helloWorld.js';
+
+describe('helloWorld', () => {
+  it('should return wrong string', () => {
+    expect(helloWorld()).toBe('Hello, Universe!'); // sengaja salah
+  });
+});
